4c6fde4268e09770792b3a447e25b4b4f9a943e2
Gowthaman Aravindan <gowthaman169@gmail.com>
2022-06-24 18:23:48 +0800

    Fetch data from MongoDB

--- backend-files/seeder.js
+++ backend-files/seeder.js
@@ -1,48 +1,47 @@
-import mongoose from "mongoose";
-import dotenv from 'dotenv'
-import colors from "colors"
+import dotenv from "dotenv";
 import users from "./data/users.js";
 import products from "./data/products.js";
-import User from "./models/userModels.js";
-import Order from "./models/userOrders.js";
-import Product from "./models/userProducts.js";
+import User from "./models/userModel.js";
+import Order from "./models/orderModel.js";
+import Product from "./models/productModel.js";
 import connectDB from "./config/db.js";
+import colors from "colors"; // eslint-disable-line
 
 dotenv.config();
 
 connectDB();
 
 const importData = async () => {
-    try {
-        await Order.deleteMany();
-        await User.deleteMany();
-        await Product.deleteMany();
-        const createdUsers = await User.insertMany(users);
-        const adminUser = createdUsers[0]._id;
-        const sampleProducts = products.map((product) => ({
-            ...product, 
-            user: adminUser, 
-        }));
-        await Product.insertMany(sampleProducts);
-        console.log("Data imported".green.inverse);
-        process.exit();
-    } catch (error) {
-        console.log(`${error}`.red.inverse);
-        process.exit(1);
-    }
-}
+  try {
+    await Order.deleteMany();
+    await User.deleteMany();
+    await Product.deleteMany();
+    const createdUsers = await User.insertMany(users);
+    const adminUser = createdUsers[0]._id;
+    const sampleProducts = products.map((product) => ({
+      ...product,
+      user: adminUser,
+    }));
+    await Product.insertMany(sampleProducts);
+    console.log("Data imported".green.inverse);
+    process.exit();
+  } catch (error) {
+    console.log(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
 
 const destroyData = async () => {
-    try {
-        await Order.deleteMany();
-        await User.deleteMany();
-        await Product.deleteMany();
-        console.log("Data destroyed".red.inverse);
-        process.exit();
-    } catch (error) {
-        console.log(`${error}`.red.inverse);
-        process.exit(1);
-    }
-}
+  try {
+    await Order.deleteMany();
+    await User.deleteMany();
+    await Product.deleteMany();
+    console.log("Data destroyed".red.inverse);
+    process.exit();
+  } catch (error) {
+    console.log(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
 
-process.argv[2] === "-d" ? destroyData() : importData(); 
+process.argv[2] === "-d" ? destroyData() : importData();
--- backend-files/server.js
+++ backend-files/server.js
@@ -1,26 +1,28 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import products from './data/products.js'
-import connectDB from "./config/db.js"
-import colors from "colors"
+import express from "express";
+import dotenv from "dotenv";
+import productRoutes from "./routes/productRoutes.js";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/error.js";
+import colors from "colors"; // eslint-disable-line
 
-dotenv.config()
+dotenv.config();
+connectDB();
 
-connectDB()
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
-
-const PORT = process.env.PORT || 5000
-app.listen(5000, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+const PORT = process.env.PORT || 5000;
+app.listen(
+  5000,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
+);
